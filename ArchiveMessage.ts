3c2528445afc0efe76721c87a10d3ab100e01e21
akimasa-l <liumingzheng8@outlook.com>
2021-08-31 18:19:28 +0900

    1. シートには存在しない新しくできたchannelを見る

--- ArchiveMessage.ts
+++ ArchiveMessage.ts
@@ -5,35 +5,88 @@ const USER_INFO_BASE_URL = "https://slack.com/api/users.info";
 
 const REPLY_LIST_BASE_URL = "https://slack.com/api/conversations.replies";
 
-const ATTACHMENTS_FOLDER = DriveApp.getFolderById("1cys1At9ByVNlx6KAOAyO8W28o9ZShC1e");
+const ATTACHMENTS_FOLDER = DriveApp.getFolderById(
+  "1cys1At9ByVNlx6KAOAyO8W28o9ZShC1e"
+);
 
 var properties = PropertiesService.getScriptProperties();
-const CHANNNEL_ADMIN_AUTH_TOKEN = properties.getProperty("CHANNNEL_ADMIN_AUTH_TOKEN");
+const CHANNNEL_ADMIN_AUTH_TOKEN = properties.getProperty(
+  "CHANNNEL_ADMIN_AUTH_TOKEN"
+);
 const BACKUP_SHEET_ID = properties.getProperty("BACKUP_SHEET_ID");
+
+interface Channel {
+  name: string;
+  id: string;
+  isPrivate: boolean;
+  topic: string;
+  // { "name": channel.name, "id": channel.id, "isPrivate": channel.is_private, "topic": channel.topic.value }
+}
 
 var usersInfo = {};
 
 function updateArchives() {
-  var ss = SpreadsheetApp.openById(BACKUP_SHEET_ID);
-  var ss_main = ss.getSheetByName("メイン");
-  var ss_mainURL = getNewSheetURL(ss, ss_main);
-  var channelList = getAllChannels();
-  var oldChannelList = ss.getRange('B2:C').filter(v => v[0]);
-  let i = 0, j = 0;
-  while (i < channelList.length) {
-    if (channelList[i].id != oldChannelList[j][0]) {
-      
-    }
+  const ss = SpreadsheetApp.openById(BACKUP_SHEET_ID);
+  const ss_main = ss.getSheetByName("メイン");
+  const channelList = getAllChannels();
+  const oldChannelList = new Set(
+    ss
+      .getRange("B2:C")
+      .getValues()
+      .filter((v) => v[0]) as unknown as string[]
+  );
+  for (const channel of channelList) {
+    if (oldChannelList.has(channel.id)) continue;
+    createChannelSheet(ss,ss_main,channel)
   }
 }
 
+function createChannelSheet(
+  ss: GoogleAppsScript.Spreadsheet.Spreadsheet,
+  ss_main: GoogleAppsScript.Spreadsheet.Sheet,
+  channel: Channel
+) {
+  const channelSheet = ss.insertSheet(channel.name);
+  const channelSheetURL = getNewSheetURL(ss, channelSheet);
+  const ss_mainURL = getNewSheetURL(ss, ss_main);
+  ss_main
+    .getRange(ss_main.getLastRow() + 1, 1, 1, 5)
+    .setValues([
+      [
+        channel.isPrivate ? "" : "〇",
+        channel.id,
+        channel.name,
+        channel.topic,
+        `=HYPERLINK("${channelSheetURL}", "リンク＞")`,
+      ],
+    ]);
+  channelSheet.getRange(1, 1, 2, 7).setValues([
+    [`=HYPERLINK("${ss_mainURL}", "＜メインへ戻る")`, "", "", "", "", "", ""],
+    [
+      "発言者",
+      "発言内容",
+      "スレッドリンク",
+      "添付ファイル",
+      "リアクション",
+      "ts",
+      "userID",
+    ],
+  ]);
+  getAllMessageInChannel(ss, channel.id, channelSheet, channelSheetURL);
+  decorateCells(channelSheet);
+  cutBlankCells(channelSheet);
+}
 
 function backUp() {
   var ss = SpreadsheetApp.openById(BACKUP_SHEET_ID);
   deleteSheets(ss);
   var ss_main = ss.insertSheet("メイン");
   var ss_mainURL = getNewSheetURL(ss, ss_main);
-  ss_main.getRange(1, 1, 1, 5).setValues([["パブリック", "チャンネルID", "チャンネル名", "トピック", "リンク"]]);
+  ss_main
+    .getRange(1, 1, 1, 5)
+    .setValues([
+      ["パブリック", "チャンネルID", "チャンネル名", "トピック", "リンク"],
+    ]);
   var channelsList = getAllChannels();
   var channelNum = 0;
   for (var channelInfo of channelsList) {
@@ -41,16 +94,29 @@ function backUp() {
       channelName = channelInfo.name;
     var channelSheet = ss.insertSheet(channelName);
     var channelSheetURL = getNewSheetURL(ss, channelSheet);
-    ss_main.getRange(ss_main.getLastRow() + 1, 1, 1, 5).setValues([[
-      channelInfo.isPrivate ? "" : "〇",
-      channelId,
-      channelName,
-      channelInfo.topic,
-      `=HYPERLINK("${channelSheetURL}", "リンク＞")`
-    ]]);
+    ss_main
+      .getRange(ss_main.getLastRow() + 1, 1, 1, 5)
+      .setValues([
+        [
+          channelInfo.isPrivate ? "" : "〇",
+          channelId,
+          channelName,
+          channelInfo.topic,
+          `=HYPERLINK("${channelSheetURL}", "リンク＞")`,
+        ],
+      ]);
     channelSheet.getRange(1, 1, 2, 7).setValues([
       [`=HYPERLINK("${ss_mainURL}", "＜メインへ戻る")`, "", "", "", "", "", ""],
-      ["発言者", "発言内容", "スレッドリンク", "添付ファイル", "リアクション", "ts", "userID"]]);
+      [
+        "発言者",
+        "発言内容",
+        "スレッドリンク",
+        "添付ファイル",
+        "リアクション",
+        "ts",
+        "userID",
+      ],
+    ]);
     getAllMessageInChannel(ss, channelId, channelSheet, channelSheetURL);
     decorateCells(channelSheet);
     cutBlankCells(channelSheet);
@@ -69,22 +135,34 @@ function backUpContinue() {
   for (var channelInfo of channelsList.slice(channelIndex)) {
     var channelId = channelInfo.id,
       channelName = channelInfo.name;
-    var oldSheet = ss.getSheetByName(channelName)
+    var oldSheet = ss.getSheetByName(channelName);
     if (oldSheet) {
       ss.deleteSheet(oldSheet);
     }
     var channelSheet = ss.insertSheet(channelName);
     var channelSheetURL = getNewSheetURL(ss, channelSheet);
-    ss_main.getRange(channelIndex + 2, 1, 1, 5).setValues([[ // backUp()で失敗したchannelから
-      channelInfo.isPrivate ? "" : "〇",
-      channelId,
-      channelName,
-      channelInfo.topic,
-      `=HYPERLINK("${channelSheetURL}", "リンク＞")`
-    ]]);
+    ss_main.getRange(channelIndex + 2, 1, 1, 5).setValues([
+      [
+        // backUp()で失敗したchannelから
+        channelInfo.isPrivate ? "" : "〇",
+        channelId,
+        channelName,
+        channelInfo.topic,
+        `=HYPERLINK("${channelSheetURL}", "リンク＞")`,
+      ],
+    ]);
     channelSheet.getRange(1, 1, 2, 7).setValues([
       [`=HYPERLINK("${ss_mainURL}", "＜メインへ戻る")`, "", "", "", "", "", ""],
-      ["発言者", "発言内容", "スレッドリンク", "添付ファイル", "リアクション", "ts", "userID"]]);
+      [
+        "発言者",
+        "発言内容",
+        "スレッドリンク",
+        "添付ファイル",
+        "リアクション",
+        "ts",
+        "userID",
+      ],
+    ]);
     getAllMessageInChannel(ss, channelId, channelSheet, channelSheetURL);
     decorateCells(channelSheet);
     cutBlankCells(channelSheet);
@@ -95,14 +173,22 @@ function backUpContinue() {
 }
 
 function getAllChannels() {
-  var res = requestToSlackAPI(CHANNNEL_LIST_BASE_URL, { "limit": 100, "types": "public_channel, private_channel" });
+  var res = requestToSlackAPI(CHANNNEL_LIST_BASE_URL, {
+    limit: 100,
+    types: "public_channel, private_channel",
+  });
   console.log(res);
-  var channelsList = [];
+  var channelsList: Channel[] = [];
   if (!res.ok) return;
   var channels = res.channels;
   for (var channel of channels) {
     if (channel.is_archived) continue; // アーカイブされたものの除外
-    channelsList.push({ "name": channel.name, "id": channel.id, "isPrivate": channel.is_private, "topic": channel.topic.value });
+    channelsList.push({
+      name: channel.name,
+      id: channel.id,
+      isPrivate: channel.is_private,
+      topic: channel.topic.value,
+    });
   }
   channelsList.sort(function (a, b) {
     if (a.name < b.name) return -1;
@@ -112,9 +198,14 @@ function getAllChannels() {
   return channelsList;
 }
 
-function getAllMessageInChannel(ss, testChannelID, channelSheet, channelSheetURL) {
+function getAllMessageInChannel(
+  ss: GoogleAppsScript.Spreadsheet.Spreadsheet,
+  testChannelID: string,
+  channelSheet: GoogleAppsScript.Spreadsheet.Sheet,
+  channelSheetURL: string
+) {
   var hasMore = true;
-  var option = { "channel": testChannelID, "limit": 3000 };
+  var option = { channel: testChannelID, limit: 3000 };
   while (hasMore) {
     var res = requestToSlackAPI(CHANNNEL_HISTORY_BASE_URL, option);
     if (!res.ok) return;
@@ -125,12 +216,12 @@ function getAllMessageInChannel(ss, testChannelID, channelSheet, channelSheetURL
     var messages = res.messages;
     var messageList = [];
     for (var message of messages) {
-      console.log(message)
+      console.log(message);
       var files = message.files;
       var fileUrls = [];
       if (!(files === undefined)) {
         for (var file of files) {
-          if (file.mode == 'tombstone' || file.mode == 'hidden_by_limit') {
+          if (file.mode == "tombstone" || file.mode == "hidden_by_limit") {
             continue;
           }
           var fileName = `${testChannelID}_${message.ts}_${file.name}`;
@@ -144,9 +235,12 @@ function getAllMessageInChannel(ss, testChannelID, channelSheet, channelSheetURL
       var user = message.user;
       var reactions = message.reactions;
       if (usersInfo[user] == undefined) {
-        var userInfo = requestToSlackAPI(USER_INFO_BASE_URL, { "user": user });
+        var userInfo = requestToSlackAPI(USER_INFO_BASE_URL, { user: user });
         if (userInfo.user == undefined) continue;
-        usersInfo[user] = (userInfo.user.profile.display_name == "") ? userInfo.user.real_name : userInfo.user.profile.display_name;
+        usersInfo[user] =
+          userInfo.user.profile.display_name == ""
+            ? userInfo.user.real_name
+            : userInfo.user.profile.display_name;
       }
       var flag = true;
       while (flag) {
@@ -158,29 +252,54 @@ function getAllMessageInChannel(ss, testChannelID, channelSheet, channelSheetURL
         }
         var mentionUser = text.slice(textPoint + 2, textPoint + 13);
         if (usersInfo[mentionUser] == undefined) {
-          var userInfo = requestToSlackAPI(USER_INFO_BASE_URL, { "user": mentionUser });
-          usersInfo[mentionUser] = (userInfo.user.profile.display_name == "") ? userInfo.user.real_name : userInfo.user.profile.display_name;
+          var userInfo = requestToSlackAPI(USER_INFO_BASE_URL, {
+            user: mentionUser,
+          });
+          usersInfo[mentionUser] =
+            userInfo.user.profile.display_name == ""
+              ? userInfo.user.real_name
+              : userInfo.user.profile.display_name;
         }
-        text = text.slice(0, textPoint) + `@${usersInfo[mentionUser]}` + text.slice(textPoint + 14);
+        text =
+          text.slice(0, textPoint) +
+          `@${usersInfo[mentionUser]}` +
+          text.slice(textPoint + 14);
       }
       messageList.push(`【${usersInfo[user]}】\n${text}`);
-      var threadURL = getAllReplyInMessage(ss, testChannelID, message.ts, channelSheetURL);
-      channelSheet.getRange(channelSheet.getLastRow() + 1, 1, 1, 7).setValues([[
-        usersInfo[user],
-        text,
-        threadURL != "" ? `=HYPERLINK("${threadURL}", "リンク＞")` : "",
-        fileUrls.join(", "),
-        (reactions != undefined)? `{ "reactions": ${JSON.stringify(reactions)} }` : "",
+      var threadURL = getAllReplyInMessage(
+        ss,
+        testChannelID,
         message.ts,
-        user]])
+        channelSheetURL
+      );
+      channelSheet
+        .getRange(channelSheet.getLastRow() + 1, 1, 1, 7)
+        .setValues([
+          [
+            usersInfo[user],
+            text,
+            threadURL != "" ? `=HYPERLINK("${threadURL}", "リンク＞")` : "",
+            fileUrls.join(", "),
+            reactions != undefined
+              ? `{ "reactions": ${JSON.stringify(reactions)} }`
+              : "",
+            message.ts,
+            user,
+          ],
+        ]);
     }
   }
 }
 
-function getAllReplyInMessage(ss, channelID, messageTs, channelSheetURL) {
+function getAllReplyInMessage(
+  ss: GoogleAppsScript.Spreadsheet.Spreadsheet,
+  channelID: string,
+  messageTs: any,
+  channelSheetURL: string
+) {
   var hasMore = true;
   var messageList = [];
-  var option = { "channel": channelID, "ts": messageTs, "limit": 100 };
+  var option = { channel: channelID, ts: messageTs, limit: 100 };
   while (hasMore) {
     var res = requestToSlackAPI(REPLY_LIST_BASE_URL, option);
     if (!res.ok) {
@@ -201,7 +320,7 @@ function getAllReplyInMessage(ss, channelID, messageTs, channelSheetURL) {
       var fileUrls = [];
       if (!(files === undefined)) {
         for (var file of files) {
-          if (file.mode == 'tombstone' || file.mode == 'hidden_by_limit') {
+          if (file.mode == "tombstone" || file.mode == "hidden_by_limit") {
             continue;
           }
           var fileName = `${channelID}_${message.ts}_${file.name}`;
@@ -215,9 +334,12 @@ function getAllReplyInMessage(ss, channelID, messageTs, channelSheetURL) {
       var user = message.user;
       var reactions = message.reactions;
       if (usersInfo[user] == undefined) {
-        var userInfo = requestToSlackAPI(USER_INFO_BASE_URL, { "user": user });
+        var userInfo = requestToSlackAPI(USER_INFO_BASE_URL, { user: user });
         if (userInfo.user == undefined) continue;
-        usersInfo[user] = (userInfo.user.profile.display_name == "") ? userInfo.user.real_name : userInfo.user.profile.display_name;
+        usersInfo[user] =
+          userInfo.user.profile.display_name == ""
+            ? userInfo.user.real_name
+            : userInfo.user.profile.display_name;
       }
       var flag = true;
       while (flag) {
@@ -227,25 +349,45 @@ function getAllReplyInMessage(ss, channelID, messageTs, channelSheetURL) {
           flag = false;
           continue;
         }
-        var mentionUser = text.slice(textPoint + 2, textPoint + 13)
+        var mentionUser = text.slice(textPoint + 2, textPoint + 13);
         if (usersInfo[mentionUser] == undefined) {
-          var userInfo = requestToSlackAPI(USER_INFO_BASE_URL, { "user": mentionUser });
-          usersInfo[mentionUser] = (userInfo.user.profile.display_name == "") ? userInfo.user.real_name : userInfo.user.profile.display_name;
+          var userInfo = requestToSlackAPI(USER_INFO_BASE_URL, {
+            user: mentionUser,
+          });
+          usersInfo[mentionUser] =
+            userInfo.user.profile.display_name == ""
+              ? userInfo.user.real_name
+              : userInfo.user.profile.display_name;
         }
-        text = text.slice(0, textPoint) + `@${usersInfo[mentionUser]}` + text.slice(textPoint + 14);
+        text =
+          text.slice(0, textPoint) +
+          `@${usersInfo[mentionUser]}` +
+          text.slice(textPoint + 14);
       }
       messageList.unshift([
         usersInfo[user],
         text,
         fileUrls.join(", "),
-        (reactions != undefined)? `{ "reactions": ${JSON.stringify(reactions)} }` : "",
+        reactions != undefined
+          ? `{ "reactions": ${JSON.stringify(reactions)} }`
+          : "",
         message.ts,
-        user
+        user,
       ]);
     }
   }
-  messageList.unshift([`=HYPERLINK("${channelSheetURL}", "＜親チャンネルへ")`, "", "", "", "", ""], ["発言者", "発言内容", "添付ファイル", "リアクション", "ts", "userID"]);
-  var oldSheet = ss.getSheetByName(messageTs)
+  messageList.unshift(
+    [
+      `=HYPERLINK("${channelSheetURL}", "＜親チャンネルへ")`,
+      "",
+      "",
+      "",
+      "",
+      "",
+    ],
+    ["発言者", "発言内容", "添付ファイル", "リアクション", "ts", "userID"]
+  );
+  var oldSheet = ss.getSheetByName(messageTs);
   if (oldSheet) {
     ss.deleteSheet(oldSheet);
   }
@@ -256,7 +398,7 @@ function getAllReplyInMessage(ss, channelID, messageTs, channelSheetURL) {
   return getNewSheetURL(ss, threadSheet);
 }
 
-function deleteSheets(spreadsheet) {
+function deleteSheets(spreadsheet: GoogleAppsScript.Spreadsheet.Spreadsheet) {
   var sheetcount = spreadsheet.getNumSheets();
   for (var i = sheetcount; i > 1; i--) {
     var sh = spreadsheet.getSheets()[i - 1];
@@ -264,22 +406,25 @@ function deleteSheets(spreadsheet) {
   }
 }
 
-function tests() {
+/* function tests() {
   var ss = SpreadsheetApp.openById(BACKUP_SHEET_ID);
   var ss_main = ss.getSheetByName("メイン");
   ss_main.moveActiveSheet(2);
-}
+} */
 
 function test() {
   console.log(getAllChannels().slice(0));
 }
 
-function requestToSlackAPI(url, parameters) {
+function requestToSlackAPI(url: string, parameters) {
   while (true) {
     var response = UrlFetchApp.fetch(`${url}?${hashToQuery(parameters)}`, {
-      method: 'GET',
-      headers: { "Content-Type": 'application/x-www-form-urlencoded', 'Authorization': 'Bearer ' + CHANNNEL_ADMIN_AUTH_TOKEN },
-      muteHttpExceptions: true
+      method: "get",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: "Bearer " + CHANNNEL_ADMIN_AUTH_TOKEN,
+      },
+      muteHttpExceptions: true,
     });
     var response_code = response.getResponseCode();
     if (response_code === 200) {
@@ -290,9 +435,9 @@ function requestToSlackAPI(url, parameters) {
   }
 }
 
-function downloadData(url, fileName) {
+function downloadData(url: string, fileName: string) {
   var options = {
-    "headers": { 'Authorization': 'Bearer ' + CHANNNEL_ADMIN_AUTH_TOKEN }
+    headers: { Authorization: "Bearer " + CHANNNEL_ADMIN_AUTH_TOKEN },
   };
   var folder = ATTACHMENTS_FOLDER;
   try {
@@ -318,20 +463,28 @@ function hashToQuery(hashList) {
   return result.join("&");
 }
 
-function getNewSheetURL(ss, newSheet) {
+function getNewSheetURL(
+  ss: GoogleAppsScript.Spreadsheet.Spreadsheet,
+  newSheet: GoogleAppsScript.Spreadsheet.Sheet
+) {
   return ss.getUrl() + "#gid=" + newSheet.getSheetId();
 }
 
-function cutBlankCells(sh) {
+function cutBlankCells(sh: GoogleAppsScript.Spreadsheet.Sheet) {
   sh.deleteRows(sh.getLastRow() + 1, sh.getMaxRows() - sh.getLastRow() - 1);
-  sh.deleteColumns(sh.getLastColumn() + 1, sh.getMaxColumns() - sh.getLastColumn() - 1);
+  sh.deleteColumns(
+    sh.getLastColumn() + 1,
+    sh.getMaxColumns() - sh.getLastColumn() - 1
+  );
 }
 
-function decorateCells(sh) {
+function decorateCells(sh: GoogleAppsScript.Spreadsheet.Sheet) {
   var rowNum = sh.getLastRow();
   var columnNum = sh.getLastColumn();
   var allCells = sh.getRange(1, 1, rowNum, columnNum);
-  allCells.setVerticalAlignments(initQuadraticArray(rowNum, columnNum, "middle"));
+  allCells.setVerticalAlignments(
+    initQuadraticArray(rowNum, columnNum, "middle")
+  );
   allCells.setWraps(initQuadraticArray(rowNum, columnNum, false));
   sh.setColumnWidth(1, 230);
   sh.setColumnWidth(2, 1050);
@@ -343,7 +496,11 @@ function decorateCells(sh) {
   sh.setFrozenRows(2);
 }
 
-function initQuadraticArray(rowSize, columnSize, arg) {
+function initQuadraticArray(
+  rowSize: number,
+  columnSize: number,
+  arg: string | boolean
+) {
   var res = [];
   for (var i = 0; i < rowSize; i++) {
     var row = [];
@@ -359,5 +516,5 @@ function deleteSheet() {
   var name = "";
   var ss = SpreadsheetApp.openById(BACKUP_SHEET_ID);
   var ss_main = ss.getSheetByName(name);
-  ss.deleteSheet(ss_main)
+  ss.deleteSheet(ss_main);
 }
